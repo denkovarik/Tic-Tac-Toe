db6c6a57d4e183992e27d0e93932e0663a2169fe
Dennis <denkovarik123@gmail.com>
2023-06-11 20:21:55 -0700

    Began converting board to grid instead of a bunch of char variables

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -2,45 +2,17 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-enum tic_tac_toe_marker // enum for the markers 'x' 'o' and blank positions on the board
-    {
-    ttt_blank_position_1 = '1',
-    ttt_blank_position_2 = '2',
-    ttt_blank_position_3 = '3',
-    ttt_blank_position_4 = '4',
-    ttt_blank_position_5 = '5',
-    ttt_blank_position_6 = '6',
-    ttt_blank_position_7 = '7',
-    ttt_blank_position_8 = '8',
-    ttt_blank_position_9 = '9',
-    ttt_x = 10,
-    ttt_o = 11,
-    ttt_e = 12 //indicates that an error has occurred
-    };
-tic_tac_toe_marker ttt_value; // initializes variable for enum tic_tac_toe_marker
+vector<vector<char>> board = {
+	{'1','2','3'},
+	{'4','5','6'},
+	{'7','8','9'},
+};
 
-char position_1; // variables for each position on the board
-char position_2;
-char position_3;
-char position_4;
-char position_5;
-char position_6;
-char position_7;
-char position_8;
-char position_9;
-
-int position_1_taken;
-int position_2_taken;
-int position_3_taken;
-int position_4_taken;
-int position_5_taken;
-int position_6_taken;
-int position_7_taken;
-int position_8_taken;
-int position_9_taken;
+char ttt_value = '';
 
 int chosen_position = 30;
 char play_again = 'Y';
@@ -49,11 +21,14 @@ void one_player_instructions (); //Prints the instructions for the game
 void two_player_instructions ();
 void board ();
 int rand_num (int high, int low); //function that chooses random numbers for the computer
-void place_X_marker (int chosen_position);
-void place_O_marker (int chosen_position);
+void place_marker (int chosen_position, char marker);
 int computer ();
 int end_game ();
 int winner ();
+bool position_taken(int pos);
+bool all_positions_taken();
+bool player_wins(char marker);
+char board_at_position(int pos);
 
 int main ()
 {
@@ -74,41 +49,19 @@ int main ()
 
         do
         {
-            position_1_taken = 0;
-            position_2_taken = 0;
-            position_3_taken = 0;
-            position_4_taken = 0;
-            position_5_taken = 0;
-            position_6_taken = 0;
-            position_7_taken = 0;
-            position_8_taken = 0;
-            position_9_taken = 0;
-
-            position_1 = ttt_blank_position_1; // initializes each position to its respective value found in the tic-tac-toe marker enum to indicate it is blank.
-            position_2 = ttt_blank_position_2;
-            position_3 = ttt_blank_position_3;
-            position_4 = ttt_blank_position_4;
-            position_5 = ttt_blank_position_5;
-            position_6 = ttt_blank_position_6;
-            position_7 = ttt_blank_position_7;
-            position_8 = ttt_blank_position_8;
-            position_9 = ttt_blank_position_9;
-
             play_again = 'e';
 
             one_player_instructions (); //function that prints out the instructions for the game
 
             double turn_double = 0;
 
-            for (int turn = 0; chosen_position != 0 && end_game () == 0; turn ++) // each iteration of the loop represents a turn
+            for (int turn = 0; chosen_position != 0 && end_game () == 0; turn++) // each iteration of the loop represents a turn
             {
-                double half_of_turn_double = turn_double / 2; // a double (number) used to help determine is turn is even
-                                                                            // turn == turn_double
-                bool turn_is_even = half_of_turn_double == turn / 2; // bool used to help determine if the turn is even
+                bool turn_is_even = (turn % 2 == 0);
 
                 if (turn_is_even && chosen_position != 0) //decides who's turn it is
                 {
-                    ttt_value = ttt_x;
+                    ttt_value = 'X';
                     cout << "Player 1's turn \n" << endl;
                     cout << "Please choose a position to place the 'X' marker or enter '0' to exit the game." << endl;
                     cout << endl;
@@ -119,7 +72,7 @@ int main ()
                 }
                 else if (chosen_position != 0)
                 {
-                    ttt_value = ttt_o;
+                    ttt_value = 'O';
                 }
 
                 while (chosen_position < 0 || chosen_position > 9) //detects invalid entries for chosen position
@@ -131,9 +84,9 @@ int main ()
                     cout << endl;
                 }
 
-                if (ttt_value == ttt_x)
+                if (ttt_value == 'X')
                 {
-                    while ((chosen_position == position_1_taken || chosen_position == position_2_taken || chosen_position == position_3_taken || chosen_position == position_4_taken || chosen_position == position_5_taken || chosen_position == position_6_taken || chosen_position == position_7_taken || chosen_position == position_8_taken || chosen_position == position_9_taken) && chosen_position != 0)
+                    while (position_taken(chosen_position) && chosen_position != 0)
                     {
                         if (chosen_position == position_1_taken) // if statement used to determine if a position is already taken
                         {
@@ -189,50 +142,17 @@ int main ()
                         cout << endl;
                     }
 
-                    place_X_marker (chosen_position);
+                    place_marker (chosen_position, 'X');
                 }
-                else if (ttt_value == ttt_o) //computer
+                else if (ttt_value == 'O') //computer
                 {
                     computer ();
 
-                    place_O_marker(chosen_position);
+                    place_marker(chosen_position, 'O');
                 }
                 else
                 {
                     cout <<"error" << endl;
-                }
-
-                switch (chosen_position) //marks positions as taken when a marker is placed in it
-                {
-                case 1:
-                    position_1_taken = 1;
-                    break;
-                case 2:
-                    position_2_taken = 2;
-                    break;
-                case 3:
-                    position_3_taken = 3;
-                    break;
-                case 4:
-                    position_4_taken = 4;
-                    break;
-                case 5:
-                    position_5_taken = 5;
-                    break;
-                case 6:
-                    position_6_taken = 6;
-                    break;
-                case 7:
-                    position_7_taken = 7;
-                    break;
-                case 8:
-                    position_8_taken = 8;
-                    break;
-                case 9:
-                    position_9_taken = 9;
-                    break;
-                default:
-                    break;
                 }
 
                 turn_double++;
@@ -290,26 +210,6 @@ int main ()
 
         do
         {
-            position_1_taken = 0;
-            position_2_taken = 0;
-            position_3_taken = 0;
-            position_4_taken = 0;
-            position_5_taken = 0;
-            position_6_taken = 0;
-            position_7_taken = 0;
-            position_8_taken = 0;
-            position_9_taken = 0;
-
-            position_1 = ttt_blank_position_1; //
-            position_2 = ttt_blank_position_2;
-            position_3 = ttt_blank_position_3;
-            position_4 = ttt_blank_position_4;
-            position_5 = ttt_blank_position_5;
-            position_6 = ttt_blank_position_6;
-            position_7 = ttt_blank_position_7;
-            position_8 = ttt_blank_position_8;
-            position_9 = ttt_blank_position_9;
-
             play_again = 'e';
 
             two_player_instructions(); //function that prints out the instructions for the game
@@ -319,13 +219,11 @@ int main ()
             for (int turn = 0; chosen_position != 0 && end_game () == 0; turn ++)
             {
                 chosen_position = 30; // variable that represents the player's chosen position on the board
-                double half_of_turn_double = turn_double / 2; // a double (number) used to help determine is turn is even
-                                                                            // turn == turn_double
-                bool turn_is_even = half_of_turn_double == turn / 2; // bool used to help determine is the turn is even
+                bool turn_is_even = (turn % 2 == 0);
 
                 if (turn_is_even && chosen_position != 0)
                 {
-                    ttt_value = ttt_x;
+                    ttt_value = 'X';
                     cout << "Player 1's turn \n" << endl;
                     cout << "Please choose a position to place the 'X' marker or enter '0' to exit the game." << endl;
                     cout << endl;
@@ -333,7 +231,7 @@ int main ()
                 }
                 else if (chosen_position != 0)
                 {
-                    ttt_value = ttt_o;
+                    ttt_value = 'O';
                     cout << "Player 2's turn \n" << endl;
                     cout << "Please choose a position to place the 'O' marker or enter '0' to exit the game." << endl;
                     cout << endl;
@@ -353,7 +251,7 @@ int main ()
                     cout << endl;
                 }
 
-                if (ttt_value == ttt_x)
+                if (ttt_value == 'X')
                 {
                     while ((chosen_position == position_1_taken || chosen_position == position_2_taken || chosen_position == position_3_taken || chosen_position == position_4_taken || chosen_position == position_5_taken || chosen_position == position_6_taken || chosen_position == position_7_taken || chosen_position == position_8_taken || chosen_position == position_9_taken) && chosen_position != 0)
                     {
@@ -407,9 +305,9 @@ int main ()
                         cout << endl;
                     }
 
-                    place_X_marker (chosen_position);
+                    place_marker (chosen_position, 'X');
                 }
-                else if (ttt_value == ttt_o)
+                else if (ttt_value == 'O')
                 {
                     while ((chosen_position == position_1_taken || chosen_position == position_2_taken || chosen_position == position_3_taken || chosen_position == position_4_taken || chosen_position == position_5_taken || chosen_position == position_6_taken || chosen_position == position_7_taken || chosen_position == position_8_taken || chosen_position == position_9_taken) && chosen_position != 0)
                     {
@@ -464,44 +362,11 @@ int main ()
                         cout << endl;
                     }
 
-                    place_O_marker(chosen_position);
+                    place_marker(chosen_position, 'O');
                 }
                 else
                 {
                     cout <<"error" << endl;
-                }
-
-                switch (chosen_position)
-                {
-                case 1:
-                    position_1_taken = 1;
-                    break;
-                case 2:
-                    position_2_taken = 2;
-                    break;
-                case 3:
-                    position_3_taken = 3;
-                    break;
-                case 4:
-                    position_4_taken = 4;
-                    break;
-                case 5:
-                    position_5_taken = 5;
-                    break;
-                case 6:
-                    position_6_taken = 6;
-                    break;
-                case 7:
-                    position_7_taken = 7;
-                    break;
-                case 8:
-                    position_8_taken = 8;
-                    break;
-                case 9:
-                    position_9_taken = 9;
-                    break;
-                default:
-                    break;
                 }
 
                 turn_double++;
@@ -601,81 +466,44 @@ int rand_num (int high, int low)
     return rand() % (high - low + 1) + low;
 }
 
-void place_X_marker (int chosen_position)
+void place_marker (int chosen_position, char marker)
 {
     switch (chosen_position)
     {
     case 0:
         break;
     case 1:
-        position_1 = 'X';
+		board[0][0] = marker;
         break;
     case 2:
-        position_2 = 'X';
+        board[0][1] = marker;
         break;
     case 3:
-        position_3 = 'X';
+        board[0][2] = marker;
         break;
     case 4:
-        position_4 = 'X';
+        board[1][0] = marker;
         break;
     case 5:
-        position_5 = 'X';
+        board[1][1] = marker;
         break;
     case 6:
-        position_6 = 'X';
+        board[1][2] = marker;
         break;
     case 7:
-        position_7 = 'X';
+        board[2][0] = marker;
         break;
     case 8:
-        position_8 = 'X';
+        board[2][1] = marker;
         break;
     case 9:
-        position_9 = 'X';
+        board[2][2] = marker;
         break;
     default:
         break;
     }
 }
 
-void place_O_marker (int chosen_position)
-{
-    switch (chosen_position)
-    {
-    case 0:
-        break;
-    case 1:
-        position_1 = 'O';
-        break;
-    case 2:
-        position_2 = 'O';
-        break;
-    case 3:
-        position_3 = 'O';
-        break;
-    case 4:
-        position_4 = 'O';
-        break;
-    case 5:
-        position_5 = 'O';
-        break;
-    case 6:
-        position_6 = 'O';
-        break;
-    case 7:
-        position_7 = 'O';
-        break;
-    case 8:
-        position_8 = 'O';
-        break;
-    case 9:
-        position_9 = 'O';
-        break;
-    default:
-        break;
-    }
-}
 
 int computer()
 {
@@ -685,15 +513,15 @@ int computer()
 
         // to win
 
-        if (position_5 == '5' && rand_num(9,1) > 2)
+        if (board_at_position(5) == '5' && rand_num(9,1) > 2)
         {
             chosen_position = 5;
         }
-        else if (position_1 == 'O' && position_2 == 'O' && rand_num (9,1) > 2)
+        else if (board_at_position(1) == 'O' && board_at_position(2) == 'O' && rand_num (9,1) > 2)
         {
             chosen_position = 3;
         }
-        else if (position_2 == 'O' && position_3 == 'O' && rand_num (9,1) > 2)
+        else if (board_at_position(2) == 'O' && board_at_position(3) == 'O' && rand_num (9,1) > 2)
         {
             chosen_position = 1;
         }
@@ -894,9 +722,9 @@ int computer()
 int end_game()
 {
     int end_game;
-    bool all_positions_are_taken = (position_1 == 'X' || position_1 == 'O') && (position_2 == 'X' || position_2 == 'O') && (position_3 == 'X' || position_3 == 'O') && (position_4 == 'X' || position_4 == 'O') && (position_5 == 'X' || position_5 == 'O') && (position_6 == 'X' || position_6 == 'O') && (position_7 == 'X' || position_7 == 'O') && (position_8 == 'X' || position_8 == 'O') && (position_9 == 'X' || position_9 == 'O');
-    bool player_1_wins = (position_1 == 'X' && position_2 == 'X' && position_3 == 'X') || (position_4 == 'X' && position_5 == 'X' && position_6 == 'X') || (position_7 == 'X' && position_8 == 'X' && position_9 == 'X') || (position_1 == 'X' && position_4 == 'X' && position_7 == 'X') || (position_2 == 'X' && position_5 == 'X' && position_8 == 'X') || (position_3 == 'X' && position_6 == 'X' && position_9 == 'X') || (position_1 == 'X' && position_5 == 'X' && position_9 == 'X') || (position_3 == 'X' && position_5 == 'X' && position_7 == 'X');
-    bool player_2_wins = (position_1 == 'O' && position_2 == 'O' && position_3 == 'O') || (position_4 == 'O' && position_5 == 'O' && position_6 == 'O') || (position_7 == 'O' && position_8 == 'O' && position_9 == 'O') || (position_1 == 'O' && position_4 == 'O' && position_7 == 'O') || (position_2 == 'O' && position_5 == 'O' && position_8 == 'O') || (position_3 == 'O' && position_6 == 'O' && position_9 == 'O') || (position_1 == 'O' && position_5 == 'O' && position_9 == 'O') || (position_3 == 'O' && position_5 == 'O' && position_7 == 'O');
+    bool all_positions_are_taken = all_positions_taken();
+    bool player_1_wins = player_wins('X');
+    bool player_2_wins = player_wins('O');
 
     if (player_1_wins)
     {
@@ -917,9 +745,168 @@ int end_game()
     return end_game;
 }
 
+bool position_taken(int pos)
+{
+	int row, col;
+	
+	if(pos < 1 || pos > 9)
+	{
+		return false;
+	}
+	
+	pair<int, int> pos_rol_col = position_2_row_col_cnvrt(pos);
+	
+	if(board[pos_rol_col.first][pos_rol_col.second] != 'X' && board[pos_rol_col.first][pos_rol_col.second] != 'O')
+	{
+		return false;
+	}
+	
+	return true;
+}
 
+pair<int, int> position_2_row_col_cnvrt(int pos)
+{
+	pair<int, int> grid_row_col;
+	switch (chosen_position) 
+	{
+		case 1:
+			grid_row_col.first = 0;
+			grid_row_col.second = 0;
+			break;
+		case 2:
+			grid_row_col.first = 0;
+			grid_row_col.second = 1;
+			break;
+		case 3:
+			grid_row_col.first = 0;
+			grid_row_col.second = 2;
+			break;
+		case 4:
+			grid_row_col.first = 1;
+			grid_row_col.second = 0;
+			break;
+		case 5:
+			grid_row_col.first = 1;
+			grid_row_col.second = 1;
+			break;
+		case 6:
+			grid_row_col.first = 1;
+			grid_row_col.second = 2;
+			break;
+		case 7:
+			grid_row_col.first = 2;
+			grid_row_col.second = 0;
+			break;
+		case 8:
+			grid_row_col.first = 2;
+			grid_row_col.second = 1;
+			break;
+		case 9:
+			grid_row_col.first = 2;
+			grid_row_col.second = 2;
+			break;
+		default:
+			grid_row_col.first = -1;
+			grid_row_col.second = -1;
+			break;
+	}
+}
 
+bool all_positions_taken()
+{
+	for(int i = 0; i < 3; i++) 
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			if(board[i][j] != 'X' && board[i][j] != 'O')
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
 
+bool player_wins(char marker)
+{
+	bool wins; 
+	
+	for(int i = 0; i < 3; i++)
+	{
+		wins = true;
+		for(int j = 0; j < 3; j++)
+		{
+			if(board[i][j] != marker)
+			{
+				wins = false;
+			}
+			
+			if(wins)
+			{
+				return true;
+			}
+		}
+	}
+	
+	for(int j = 0; j < 3; j++)
+	{
+		wins = true;
+		for(int i = 0; i < 3; i++)
+		{
+			if(board[i][j] != marker)
+			{
+				wins = false;
+			}
+			
+			if(wins)
+			{
+				return true;
+			}
+		}
+	}
+	
+	wins = true;
+	for(int i = 0; i < 3; i++)
+	{
+		if(board[i][i] != marker)
+		{
+			wins = false;
+		}
+			
+		if(wins)
+		{
+			return true;
+		}
+	}
+	
+	wins = true;
+	for(int i = 0; i < 3; i++)
+	{
+		if(board[i][2-i] != marker)
+		{
+			wins = false;
+		}
+			
+		if(wins)
+		{
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+char board_at_position(int pos)
+{
+	if(pos < 1 || pos > 9)
+	{
+		return 'e';
+	}
+	
+	pair<int, int> row_col = position_2_row_col_cnvrt(pos);
+	
+	return board[row_col.first][row_col.second];
+}
 
 
 
